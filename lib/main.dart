eedf52853140978c8cdb097ffbc5c222c7d332ce
guivconB <guivcon@gmail.com>
2025-05-01 23:27:35 -0300

    Tela de introdução(Falta levar para a proxima tela)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:heartsync/src/features/Menu/presentation/view/statistic_screen.dart';
 import 'package:heartsync/src/features/home/presentation/view/Home_screen.dart';
-import 'package:heartsync/src/features/login/presentation/view/Profile_screen.dart';
 
 import 'Introducao.dart';
 
@@ -16,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: StatisticScreen(),
+      home: Introducao(),
     );
   }
 }
